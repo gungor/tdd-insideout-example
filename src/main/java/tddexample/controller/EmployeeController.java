56cbff6950065c8e45ee800e5c2bbfaf09adc0c2
gungor <gungor.yilmaz@vodafone.com>
2021-04-18 20:36:47 +0300

    Cycle 10: GREEN, Controller: update employee

--- src/main/java/tddexample/controller/EmployeeController.java
+++ src/main/java/tddexample/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package tddexample.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import tddexample.model.entity.Employee;
 import tddexample.model.rest.EmployeeSaveRequest;
+import tddexample.model.rest.EmployeeUpdateRequest;
 import tddexample.service.EmployeeService;
 
 @RestController
@@ -19,6 +21,11 @@ public class EmployeeController {
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody EmployeeSaveRequest request) {
         return employeeService.saveEmployee(request);
+    }
+
+    @PutMapping("/employees")
+    Employee updateEmployee(@RequestBody EmployeeUpdateRequest request) {
+        return employeeService.updateEmployee(request);
     }
 
 }
