b24460c10d8fa2831f07da26c7e9d9cddc65f21f
gungor <gungor.yilmaz@vodafone.com>
2021-04-18 03:41:31 +0300

    Cycle 1: GREEN, Repository: search by name

--- src/main/java/tddexample/repository/EmployeeRepository.java
+++ src/main/java/tddexample/repository/EmployeeRepository.java
@@ -3,7 +3,9 @@ package tddexample.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tddexample.model.entity.Employee;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
-
+    Optional<Employee> findByFullName(String fullName);
 }
