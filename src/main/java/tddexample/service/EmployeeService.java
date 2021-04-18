460b7efd9e5660aa09c150295a85bfe986359a07
gungor <gungor.yilmaz@vodafone.com>
2021-04-18 18:16:32 +0300

    Cycle 8: GREEN, Service: get employee by name throws exception when employee not found by name

--- src/main/java/tddexample/service/EmployeeService.java
+++ src/main/java/tddexample/service/EmployeeService.java
@@ -35,7 +35,8 @@ public class EmployeeService {
     }
 
     public Employee getEmployeeByName(String fullName) {
-        return employeeRepository.findByFullName(fullName).get();
+        return employeeRepository.findByFullName(fullName)
+                .orElseThrow(() -> new EmployeeNotFoundException());
     }
 
 }
